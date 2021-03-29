b1a0c443e997ad47b84f91ddcada18e25fff3e79
Godwin <gtsado94@gmail.com>
2021-03-29 11:23:29 -0700

    "Comment out seaborn"

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 #mport numpy_financial as npf
 import pandas as pd
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import streamlit as st
 import streamlit.components.v1 as stc 
 import copy
